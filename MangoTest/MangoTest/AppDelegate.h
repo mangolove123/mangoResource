d493456582d383a111baf96db5c9b1499a1f4a8f
mango <wangmengfa@meituan.com>
2016-12-24 11:00:28 +0800

    添加测试代码

---
+++ MangoTest/MangoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MangoTest
+//
+//  Created by Mango on 2016/12/24.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MangoTest/MangoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MangoTest
+//
+//  Created by Mango on 2016/12/24.
+//  Copyright © 2016年 Mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
