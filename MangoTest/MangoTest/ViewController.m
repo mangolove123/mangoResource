799b99e5949f6d56d0423c28a0d2055dddd2c1fa
王孟发 <wangmengfa@meituan.com>
2016-12-25 22:17:15 +0800

    master上的修改，其实没有啥

--- MangoTest/MangoTest/ViewController.m
+++ MangoTest/MangoTest/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 //    NSLog(@"什么意思");
+    NSLog(@"我爱你啊!");
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
